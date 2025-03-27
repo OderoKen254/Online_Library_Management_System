d15d49a29f62a2e556bf1add1fe4fef6abe5d738
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-03-27 08:48:52 +0300

    initialize app and catalog load

--- main.js
+++ main.js
@@ -3,4 +3,18 @@
 const Books_API_URL =
 let users = JSON.parse(localStorage.getItem("users")) || [];
 let books = JSON.parse(localStorage.getItem("books")) || [];
-const currentUsers = null;
+const currentUser = null;
+
+// initialize the app
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("currentUser")) {
+        currentUser = JSON.parse(localStorage.getItem("currentUser"));
+        document.getElementById('logged-in-user').textContent = `Welcome, ${currentUser.username}, login successful!`;
+        document.getElementById('logout-btn').style.display = 'inline';
+        document.getElementById('manage-tab').style.display = currentUser.isAdmin ? 'inline' : 'none';
+        displayManageBooks();
+    }
+    // to initialize catalog load
+    displayBooks();
+});
