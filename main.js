47c8ea02c7270c02736033c29b280b3b33efe665
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-03-27 08:29:42 +0300

    add element references in js file

---
+++ main.js
@@ -0,0 +1,6 @@
+// Element references
+
+const Books_API_URL =
+let users = JSON.parse(localStorage.getItem("users")) || [];
+let books = JSON.parse(localStorage.getItem("books")) || [];
+const currentUsers = null;
